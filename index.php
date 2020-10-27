bcbfe286e786cb760c058b6ca84a79ec754dce80
osikoraa <42960719+osikoraa@users.noreply.github.com>
2020-10-27 13:54:22 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -39,6 +39,54 @@
 
 ?>
 </main>
+<aside>
+
+                    <h2>Insert Autor:</h2>
+                    <form action="autor.php" method="POST">
+                        <input type="text" name="imie">
+                        <input type="text" name="nazwisko"><br>
+                        <input type="submit" value="Dodaj">
+                    </form>
+                    <h2>Insert Tytuł:</h2>
+                    <form action="tytul.php" method="POST">
+                        <input type="text" name="tytul"><br>
+                        <input type="submit" value="Dodaj">
+                    </form>
+                    <h2>Autor i Tytuł:</h2>
+                    <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "library";
+
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+
+                        $result2 = $conn->query("SELECT * FROM tytuly");
+                        
+
+                        echo("<form action='books.php' method='POST'>");
+                        echo("<select name='tytuly'>");
+                        while($row=$result2->fetch_assoc() ){
+                            echo("<option value='".$row['id_tytul']."'>".$row['tytul']."</option>");
+                        }
+                        echo("</select>");
+
+                        $result3 = $conn->query("SELECT * FROM autorzy");
+                       
+                        echo("<select name='autorzy'>");
+                        while($row=$result3->fetch_assoc() ){
+                            echo("<option value='".$row['id_autor']."'>".$row['imie']." ".$row['nazwisko']."</option>");
+                        }
+                        echo("</select>");
+                        echo("<br>");
+                        echo("<input type='submit' value='Dodaj'>");
+                        echo("</form>");
+
+                     
+                        
+                    ?>
+
+</aside>
 <footer></footer>
 </body>
 <script src="main.js"></script>
