4e310bf9c4aa0eed1eb542b525e6ecf4b979ca7d
osikoraa <42960719+osikoraa@users.noreply.github.com>
2020-11-09 15:30:00 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Logowanie</title>
+    <link rel="stylesheet" href="login.css">
+
+</head>
+
+<body>
+
+
+
+<div>
+<?php
+
+session_start();
+
+if( isset($_GET['akcja']) && $_GET['akcja'] == "wyloguj" ){
+    unset($_SESSION['logowanie']);
+}
+
+if( !isset($_SESSION['logowanie']) ){
+?>  
+
+    <form action="index.php" method="post" class="logowanie">
+        <input type="text" name="username" placeholder="username">
+        <input type="text" name="password" placeholder="password">
+        <input type="submit" value="Zaloguj">
+    </form>
+<?php
+}else{
+    ?>
+    <div class="login">
+    <h1 class='zalogowany'>ZALOGOWANY</h1>
+    <button><a href='index.php?akcja=wyloguj' class="btn-wyloguj">WYLOGUJ</a></button>
+    </div>
+    <?php
+}
+
+?>
+</div>
+
+</body>
+</html>
+
