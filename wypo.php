67b5b32247b510920484ad141648457c16b5f94b
osikoraa <42960719+osikoraa@users.noreply.github.com>
2020-11-12 13:45:42 +0100

    Add files via upload

---
+++ wypo.php
@@ -0,0 +1,22 @@
+<?php
+    $servername = "remotemysql.com";
+    $username = "IzKON2j8qa";
+    $password = "NNn2mEn1AB";
+    $dbname = "IzKON2j8qa";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $tytul=$_POST['tytuly'];
+    $user=$_POST['users'];
+
+    $sql = "INSERT INTO `wypo`(`id`, `id_tytul`, `id_user`,`data_wyp`,`data_od`) VALUES (NULL,'$tytul','$user',curdate(),NULL)";
+ 
+    mysqli_query($conn, $sql);
+
+    mysqli_close($conn);
+
+    header("Location:https://sikora-oliwier-wdw.herokuapp.com/");
+    
+
+
+?>
