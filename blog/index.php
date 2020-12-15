6b8cd8296140b5f11f0743d968d7c4ab74236b7b
osikoraa <42960719+osikoraa@users.noreply.github.com>
2020-12-15 16:46:40 +0100

    Add files via upload

--- blog/index.php
+++ blog/index.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<div class="header"></div>
  
  <div class="main">
         <?php
@@ -16,17 +18,19 @@
              $dbname = "IzKON2j8qa";
      
          $conn = new mysqli($servername, $username, $password, $dbname);
-         $result=$conn->query("SELECT posty.content, GROUP_CONCAT(tagi.tag) as tagi FROM posty,tagi,post_tag WHERE posty.id_post=post_tag.id_post AND tagi.id_tag=post_tag.id_tag GROUP by posty.content");
-         echo("<table border='1'>
-         <th>Post</th>
-         <th>Tagi</th>
-         ");
+         $result=$conn->query("SELECT posty.content, GROUP_CONCAT(tagi.tag) as tagi FROM posty,tagi,post_tag WHERE posty.id_post=post_tag.id_post AND tagi.id_tag=post_tag.id_tag GROUP by posty.content");    
          while($row=$result->fetch_assoc()){
-             echo("<tr>");
-             echo("<td>".$row['content']."</td><td>".$row['tagi']."</td>");
-             echo("</tr>");
-         };
-         echo("</table>");
+            echo("<article class='post'>");
+            echo("<div class='content'>");
+            echo($row['content']);
+            echo("</div>");
+            echo("<h3 class='tag'>");
+                
+                    echo("#".$row['tagi']);
+
+            echo("</h3>");
+            echo("</article>");
+        }     
         ?>
  </div>
 
@@ -38,6 +42,23 @@
                     <input type="text" name="tag"  placeholder="tag"> <br>
                     <input type="submit" value="Dodaj">
                     </form>  <br> <br>
+
+                    <h2>Wybierz szukany tag:</h2>
+                    <?php
+             $servername = "remotemysql.com";
+             $username = "IzKON2j8qa";
+             $password = "NNn2mEn1AB";
+             $dbname = "IzKON2j8qa";
+     
+         $conn = new mysqli($servername, $username, $password, $dbname);
+         $result=$conn->query("SELECT * from tagi");    
+         while($row=$result->fetch_assoc()){
+            echo("<form action='index1.php' method='post'>");
+            echo("<input type='hidden' value='".$row['id_tag']."'>");
+            echo("<input type='submit' value='".$row['tag']."'>");
+            echo("</form>");
+        }     
+        ?>
  
  </div>
     
